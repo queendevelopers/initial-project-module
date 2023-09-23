141a6eed92264cff4f2a6879a1c2ea50d850d27f
Nikesh Maharjan <dev@nikeshs-air.myfiosgateway.com>
2023-09-23 13:29:34 -0400

    added repsonsive framework

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'config/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'core/injection/injection.dart';
-import 'package:injectable/injectable.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:super_module/super_module.dart';
 import 'package:initialproject/config/app_env.dart';
-import 'package:initialproject/config/app_config.dart';
 import 'package:initialproject/core/routes/app_router.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,7 +30,8 @@ class MyApp extends StatelessWidget {
             DeviceOrientation.portraitUp,
             DeviceOrientation.portraitDown,
           ]);
-          return FlutterEasyLoading(
+          return ResponsiveBreakpoints.builder(
+        child:   FlutterEasyLoading(
             child:
                 // ScreenUtilInit(
                 //   designSize: const Size(360,
@@ -43,7 +44,13 @@ class MyApp extends StatelessWidget {
                     routeInformationParser: _appRouter.defaultRouteParser(),
                     routerDelegate: AutoRouterDelegate(_appRouter)),
             //   ),
-          );
+          ),
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],);
         });
   }
 }
