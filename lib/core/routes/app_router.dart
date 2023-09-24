6f9828698e564704959da600a2dbb9ef0805613a
Nikesh Maharjan <dev@nikeshs-air.myfiosgateway.com>
2023-09-24 08:48:30 -0400

    added conflict files

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -2,6 +2,10 @@ import 'package:auto_route/auto_route.dart';
 import 'package:super_module/super_module.dart';
 import 'package:initialproject/core/routes/app_router.gr.dart';
 
+// @AdaptiveAutoRouter(preferRelativeImports: true, routes: [
+// ])
+// class $AppRouter {}
+
 @AutoRouterConfig()
 class AppRouter extends $AppRouter {
   final ISessionManager sessionManager;
